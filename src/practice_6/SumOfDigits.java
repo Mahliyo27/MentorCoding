bf95d26605c14d7a146382e0593f9bd3fee2fd1e
Mahliyo_27 <mahliyogafurova27@gmail.com>
2023-02-09 18:26:34 -0500

    practice5

--- src/practice_6/SumOfDigits.java
+++ src/practice_6/SumOfDigits.java
@@ -9,10 +9,15 @@ public class SumOfDigits {
 
     public static void main(String[] args) {
 
+        String str1 = "ab5c2d4ef-12s";
+        System.out.println("The sum of the digits in the String is= "+ sumOfDigits(str1));
+
 
       String n =   SumOfDigits.stringNum("10","10");
 
         System.out.println(n);
+
+        System.out.println(sumOfDigits(str1));
 
 
     }
@@ -28,6 +33,82 @@ public class SumOfDigits {
         result +=  sum1 + sum;
 
        return result;
+
+    }
+
+
+
+
+        // return the sum of the digits present in the given String
+
+        public static int sumOfDigits(String str){
+            int n = str.length();
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++){
+                if (Character.isDigit(str.charAt(i))){
+                    String given = str.substring(i, i+1);
+                    sum += Integer.parseInt(given);
+                }
+            }
+            return sum;
+        }
+
+
+
+
+
+
+
+
+
+    public static int sumOfDigits_s1(String str) {
+
+        int n = str.length();
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (Character.isDigit(str.charAt(i))) {
+
+                if (str.contains("-")) {
+
+                    sum += Integer.parseInt(str.substring(i, i + 1)) * -1;
+
+                } else {
+
+                    sum += Integer.parseInt(str.substring(i, i + 1));
+                }
+
+            }
+
+        }
+        return sum;
+    }
+
+
+    public static int digit_sum_string (String str){
+
+        int num = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (Character.isDigit(str.charAt(i))) {
+                if (str.startsWith("-")) {
+
+                    num += Integer.valueOf("" + str.charAt(i)) * -1;   // value of OR parseInt works
+
+                } else {
+                    String s = "" + str.charAt(i);
+                    num += Integer.parseInt("" + str.charAt(i));
+                }
+
+            }
+        }
+
+        return num;
 
     }
 
--- src/practice_6/validationPassword.java
+++ src/practice_6/validationPassword.java
@@ -8,81 +8,33 @@ public class validationPassword {
     public static void main(String[] args) {
 
 
-        String password = "";
 
-        for (char i = 0; i <password.length() ; i++) {
 
-            if (i >= 'A' && i <= 'Z'){
-                System.out.println(password.charAt(i));
+    }
+        public static boolean password(String password) {
+
+            boolean specialChar = false, digit = false, upperCase = false, lowerCase = false;
+
+            if (password.length() < 6 || password.contains(" ")) {
+
+                throw new RuntimeException("It should contain at least 6 characters and no space");
 
             }
 
+            for (int i = 0; i < password.length(); i++) {
 
-    }
+                if (Character.isUpperCase(password.charAt(i))) {
+                    upperCase = true;
+                } else if (Character.isLowerCase(password.charAt(i))) {
+                    lowerCase = true;
+                } else if (Character.isDigit(password.charAt(i))) {
+                    digit = true;
+                } else {
+                    specialChar = true;
+                }
 
+            }
 
-
-
-
-//    public  static String passwordValid(String password ) {
-//
-//        //String -- Password Validation Task
-//        //1. Write a return method that can verify if a password is valid or not. require  //1. Password MUST be at least have 6 characters and should not contain space 2.
-//        // PassWord should at least contain one upper case letter
-//        //3. PassWord should at least contain one lowercase letter
-//        //4. Password should at least contain one special characters
-//        //5. Password should at least contain a digit
-//
-//
-//
-//
-//        boolean length = password.length() >= 8;
-//        boolean lower = false;
-//        boolean upper = false;
-//        boolean number = false;
-//        boolean special = false;
-//
-//
-//        for (int i = 0; i < password.length(); i++) {
-//            char c = password.charAt(i);
-//            if (c >= 97 && c <= 122) lower = true;
-//            if (c >= 65 && c <= 90) upper = true;
-//            if (c >= 48 && c <= 57) number = true;
-//            if ((c >= 33 && c <= 47) || (c >= 58 && c <= 64)
-//                    || (c >= 91 && c <= 96) || (c >= 123 && c <= 126)) special = true;
-//        }
-//        if (!length) {
-//            System.out.println("Invalid length");
-//        } else if (!lower) {
-//            System.out.println("Missing lowercase letter");
-//        } else if (!upper) {
-//            System.out.println("Missing uppercase letter");
-//        } else if (!number) {
-//            System.out.println("Missing number");
-//        } else if (!special) {
-//            System.out.println("Missing special character");
-//        } else {
-//            System.out.println("Valid password");
-//        }
-//
-//        return password;
-//
-//    }
-//
-//
-//
-//
-//    public static String validPassword(String password){
-//
-//
-
-
-
+            return specialChar && digit && upperCase && lowerCase;
         }
-
-
-
-    }
-
-
-
+        }
